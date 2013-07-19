3b6e1b5206b860082607edb96032e1cb45fb4c11
Christopher Warren <christopher.warren@gmail.com>
2013-07-19 16:12:54 -0700

    clean up tests

--- spec/runscope_spec.rb
+++ spec/runscope_spec.rb
@@ -1,85 +1,55 @@
 require 'spec_helper'
 
 describe Runscope do
-  context "basic methods" do
+  context "#version_string" do
     it 'should return correct version string' do
       Runscope.version_string.should eq("Runscope version #{Runscope::VERSION}")
     end
   end
 
-  context 'monitoring' do
-    before(:each) do
+  context '#monitor?' do
+    before(:each){
       Runscope.domains = ["api.stackexchange.com", /\S+\.desk\.com/]
-    end
-
-    after(:each) do
-      Runscope.reset
-    end
-
+    }
+    after(:each){ Runscope.reset }
     context "when enabled" do
-      before(:each) do
-        Runscope.enabled = true
-      end
-
-      it "should be true if the domain is monitored" do
-        Runscope.monitor?("api.stackexchange.com").should eq(true)
-      end
-
-      it "should be false if the domain is not monitored" do
-        Runscope.monitor?("api.twitter.com").should eq(false)
-      end
+      before{ Runscope.enabled = true }
+      it{expect(Runscope.monitor?("api.stackexchange.com")).to eq(true)}
+      it{expect(Runscope.monitor?("api.twitter.com")).to eq(false)}
     end
 
     context "when disabled" do
-      before(:each) do
-        Runscope.enabled = false
-      end
-
-      it "should be false if the domain is monitored" do
-        Runscope.monitor?("api.stackexchange.com").should eq(false)
-      end
-
-      it "should be false if the domain is not monitored" do
-        Runscope.monitor?("api.twitter.com").should eq(false)
-      end
+      before(:each){ Runscope.enabled = false }
+      it{expect(Runscope.monitor?("api.stackexchange.com")).to eq(false)}
+      it{expect(Runscope.monitor?("api.twitter.com")).to eq(false)}
     end
   end
 
-  context 'matching domains' do
-    before(:each) do
+  context '#monitor_domain?' do
+    before(:each){
       Runscope.domains = ["api.stackexchange.com", /\S+\.desk\.com/]
-    end
+    }
 
-    it "should match when stored as a string" do
-      Runscope.monitor_domain?("api.stackexchange.com").should eq(true)
-    end
-
-    it "should match when stored as a Regexp" do
-      Runscope.monitor_domain?("test.desk.com").should eq(true)
-    end
-
-    it "should not match domains that aren't set" do
-      Runscope.monitor_domain?("api.twitter.com").should eq(false)
-    end
-
+    it{expect(Runscope.monitor_domain?("api.stackexchange.com")).to eq(true)}
+    it{expect(Runscope.monitor_domain?("test.desk.com")).to eq(true)}
+    it{expect(Runscope.monitor_domain?("api.twitter.com")).to eq(false)}
     it "should raise an error if no domains are set" do
       Runscope.domains = nil
-      expect {
-        Runscope.monitor_domain?("api.stackexchange.com")
-      }.to raise_error(NoDomainsSetError)
+      expect {Runscope.monitor_domain?("api.stackexchange.com")}.to(
+        raise_error(NoDomainsSetError)
+      )
     end
   end
 
-  context 'generating a runscope proxy domain' do
+  context '#proxy_domain' do
     before(:each) do
       @bucket = "1234abcd"
       Runscope.bucket = @bucket
     end
 
-    it "should return a domain to runscope" do
-      Runscope.proxy_domain("api.stackexchange.com").should
-        eq("api-stackexchange-com-#{@bucket}.runscope.net")
-    end
+    it{expect(Runscope.proxy_domain("api.stackexchange.com")).to(
+      eq("api-stackexchange-com-#{@bucket}.runscope.net")
+    )}
 
     it "should raise an error if no bucket is set" do
       Runscope.bucket = nil
